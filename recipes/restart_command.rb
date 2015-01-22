3c3d0d613f34bff40ef18d2fce30ca24986f8c61
Dave Steinberg <dave.steinberg@hkstrategies.com>
2015-01-22 17:54:55 +0000

    check if the apps being deployed are rails apps rather than forcing the layer to be a ROR layer

--- recipes/restart_command.rb
+++ recipes/restart_command.rb
@@ -1,4 +1,8 @@
 node[:deploy].each do |application, deploy|
+  if deploy[:application_type] != 'rails'
+    Chef::Log.debug("Skipping opsworks_custom_env::restart_command for application #{application} as it is not a rails app")
+    next
+  end
 
   execute "restart Rails app #{application} for custom env" do
     cwd deploy[:current_path]
